427150cb18455f4414410c553d83dc80f23afab0
Toan Dang <ddtoan991205@gmail.com>
2019-08-03 23:40:54 +0700

    Bai 36 - Danh sach san pham, phan trang custopm

--- Model/DAO/ProductDAO.cs
+++ Model/DAO/ProductDAO.cs
@@ -20,21 +20,38 @@ namespace Model.DAO
         {
             return db.Products.OrderByDescending(x => x.CreatedDate).Take(top).ToList();
         }
-
-        public List<Product> ListFeatureProduct(int top)
-        {
-            return db.Products.Where(x => x.TopHot != null && x.TopHot > DateTime.Now).OrderByDescending(x => x.CreatedDate).Take(top).ToList();
-        }
-
         public Product ViewDetail(long id)
         {
             return db.Products.Find(id);
         }
 
+        /// <summary>
+        /// Get feature products
+        /// </summary>
+        /// <param name="top"></param>
+        /// <returns></returns>
+        public List<Product> ListFeatureProduct(int top)
+        {
+            return db.Products.Where(x => x.TopHot != null && x.TopHot > DateTime.Now).OrderByDescending(x => x.CreatedDate).Take(top).ToList();
+        }
+
+
         public List<Product> ListRelatedProducts(long productId)
         {
             var product = db.Products.Find(productId);
             return db.Products.Where(x => x.ID != productId && x.CategoryID == product.CategoryID).ToList();
+        }
+
+        /// <summary>
+        /// Get list products by category
+        /// </summary>
+        /// <param name="categoryId"></param>
+        /// <returns></returns>
+        public List<Product> ListByCategoryId(long categoryId, ref int totalRecord, int pageIndex = 1, int pageSize = 2)
+        {
+            totalRecord = db.Products.Where(x => x.CategoryID == categoryId).Count();
+            var model = db.Products.Where(x => x.CategoryID == categoryId).OrderByDescending(x => x.CreatedDate).Skip((pageIndex-1) * pageIndex).Take(pageSize).ToList();
+            return model; 
         }
     }
 }
--- OnlineShopTEDU/Controllers/ProductController.cs
+++ OnlineShopTEDU/Controllers/ProductController.cs
@@ -22,10 +22,27 @@ namespace OnlineShopTEDU.Controllers
             return PartialView(model);
         }
 
-        public ActionResult Category(long cateID)
+        public ActionResult Category(long cateID, int page = 1, int pageSize = 2)
         {
             var category = new CategoryDAO().ViewDetail(cateID);
-            return View(category);
+            ViewBag.Category = category;
+            int totalRecord = 0;
+            var model = new ProductDAO().ListByCategoryId(cateID, ref totalRecord, page, pageSize);
+
+            ViewBag.Total = totalRecord;
+            ViewBag.Page = page;
+
+            int maxPage = 5;
+            int totalPage = 0;
+            totalPage = (int)Math.Ceiling((double)(totalRecord / pageSize));
+            ViewBag.TotalPage = totalPage;
+            ViewBag.MaxPage = maxPage;
+            ViewBag.First = 1;
+            ViewBag.Last = totalPage;
+            ViewBag.Next = page + 1;
+            ViewBag.Prev = page - 1;
+ 
+            return View(model);
         }
 
         public ActionResult Detail(long id)
