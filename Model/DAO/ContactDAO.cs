a4e7ee204efb60bb001db3be266224d69d172c43
Toan Dang <ddtoan991205@gmail.com>
2019-08-19 16:40:55 +0700

    Bai 43 - Tao form lien he va tich hop Google Maps API

---
+++ Model/DAO/ContactDAO.cs
@@ -0,0 +1,31 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DAO
+{
+    public class ContactDAO
+    {
+        OnlineShopTeduDbContext db;
+
+        public ContactDAO()
+        {
+            db = new OnlineShopTeduDbContext();
+        }
+
+        public Contact GetActiveContact()
+        {
+            return db.Contacts.SingleOrDefault(x => x.Status == true);
+        }
+
+        public int Insert(Feedback fb)
+        {
+            db.Feedbacks.Add(fb);
+            db.SaveChanges();
+            return fb.ID;
+        }
+    }
+}
---
+++ OnlineShopTEDU/Controllers/ContactController.cs
@@ -0,0 +1,49 @@
+﻿using Model.DAO;
+using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopTEDU.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var model = new ContactDAO().GetActiveContact();
+            return View(model);
+        }
+
+        [HttpPost]
+        public JsonResult Send(string name, string mobile, string address, string email, string content)
+        {
+            var feedback = new Feedback();
+            feedback.Name = name;
+            feedback.Email = email;
+            feedback.Phone = mobile;
+            feedback.Content = content;
+            feedback.Address = address;
+            feedback.CreatedDate = DateTime.Now;
+
+            var id = new ContactDAO().Insert(feedback);
+
+            if (id > 0)
+            {
+                return Json(new
+                {
+                    status = true
+                });
+            }
+            else
+            {
+                return Json(new
+                {
+                    status = false
+                });
+            }
+        }
+    }
+}
