a2a3d3ed577104e70d5b4f6ac88331f3210fa601
Toan Dang <ddtoan991205@gmail.com>
2019-08-28 10:04:35 +0700

    Bai 46 - Dang Nhap bang FB

--- OnlineShopTEDU/Controllers/UserController.cs
+++ OnlineShopTEDU/Controllers/UserController.cs
@@ -1,10 +1,12 @@
 ﻿using BotDetect.Web.Mvc;
+using Facebook;
 using Model.DAO;
 using Model.EF;
 using OnlineShopTEDU.Common;
 using OnlineShopTEDU.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +15,18 @@ namespace OnlineShopTEDU.Controllers
 {
     public class UserController : Controller
     {
+        private Uri RedirectUri
+        {
+            get
+            {
+                var uriBuilder = new UriBuilder(Request.Url);
+                uriBuilder.Query = null;
+                uriBuilder.Fragment = null;
+                uriBuilder.Path = Url.Action("FacebookCallback");
+                return uriBuilder.Uri;
+            }
+        }
+
         // GET: User
         [HttpGet]
         public ActionResult Register()
@@ -23,6 +37,64 @@ namespace OnlineShopTEDU.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        public ActionResult LoginFacebook()
+        {
+            var fb = new FacebookClient();
+            var loginUrl = fb.GetLoginUrl(new
+            {
+                client_id = ConfigurationManager.AppSettings["FbAppId"],
+                client_secret = ConfigurationManager.AppSettings["FbAppSecret"],
+                redirect_uri = RedirectUri.AbsoluteUri,
+                response_type = "code",
+                scope = "email",
+            });
+
+            return Redirect(loginUrl.AbsoluteUri);
+        }
+
+        public ActionResult FacebookCallback(string code)
+        {
+            var fb = new FacebookClient();
+            dynamic result = fb.Post("oauth/access_token", new
+            {
+                client_id = ConfigurationManager.AppSettings["FbAppId"],
+                client_secret = ConfigurationManager.AppSettings["FbAppSecret"],
+                redirect_uri = RedirectUri.AbsoluteUri,
+                code = code
+            });
+
+            var accessToken = result.access_token;
+
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                fb.AccessToken = accessToken;
+                // Get the user's information, like email, first name, middle name etc
+                dynamic me = fb.Get("/me?fields=id,name,email");
+                string email = me.email;
+                string userName = me.email;
+                string name = me.name;
+
+                var user = new User();
+                user.Email = email;
+                user.UserName = email;
+                user.Status = true;
+                user.Name = name;
+                user.CreatedDate = DateTime.Now;
+
+                var resultInsert = new UserDAO().InsertFacebookUser(user);
+                if (resultInsert > 0)
+                {
+                    var userSession = new UserLogin();
+                    userSession.UserName = user.UserName;
+                    userSession.UserID = user.ID;
+
+                    Session.Add(CommonConstants.USER_SESSION, userSession);
+
+                }
+            }
+            return Redirect("/");
         }
 
         [HttpPost]
@@ -44,7 +116,7 @@ namespace OnlineShopTEDU.Controllers
                     Session.Add(CommonConstants.USER_SESSION, userSession);
 
                     return Redirect("/");
-                    
+
                 }
                 else if (result == 0)
                 {
@@ -63,7 +135,7 @@ namespace OnlineShopTEDU.Controllers
                     ModelState.AddModelError("", "Đăng nhập không thành công!");
                 }
             }
-            
+
             return View(model);
         }
 
