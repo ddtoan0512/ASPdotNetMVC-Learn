ea7bb70a1f09c6f69111fb695f11c254ee1341bc
Toan Dang <ddtoan991205@gmail.com>
2019-08-28 15:53:31 +0700

    bai 47 - Da ngon ngu

--- Model/DAO/CategoryDAO.cs
+++ Model/DAO/CategoryDAO.cs
@@ -16,6 +16,13 @@ namespace Model.DAO
             db = new OnlineShopTeduDbContext();
         }
 
+        public long Insert(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return category.ID;
+        }
+
         public List<Category> ListAll()
         {
             return db.Categories.Where(x => x.Status == true).ToList();
--- OnlineShopTEDU/Areas/Admin/Controllers/CategoryController.cs
+++ OnlineShopTEDU/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Model.DAO;
+using Model.EF;
+using OnlineShopTEDU.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +9,7 @@ using System.Web.Mvc;
 
 namespace OnlineShopTEDU.Areas.Admin.Controllers
 {
-    public class CategoryController : Controller
+    public class CategoryController : BaseController
     {
         // GET: Admin/Category
         public ActionResult Index()
@@ -17,6 +20,26 @@ namespace OnlineShopTEDU.Areas.Admin.Controllers
         public ActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category model)
+        {
+            var currentCulture = Session[CommonConstants.CurrentCulture];
+            model.Language = currentCulture.ToString();
+            if (ModelState.IsValid)
+            {
+                var id = new CategoryDAO().Insert(model);
+                if(id > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("",StaticResources.Resources.InsertCategoryFailed);
+                }
+            }
+            return View(model);
         }
     }
 }
